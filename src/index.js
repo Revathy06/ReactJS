4c0c16fc6fef8d4646c7c721198f631d5c247722
Revathy06 <49243396+Revathy06@users.noreply.github.com>
2022-12-27 14:49:16 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite</h1>
+    <ul>
+      <li>Coffee</li>
+      <li>Chocolate</li>
+      <li>Maggie</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
